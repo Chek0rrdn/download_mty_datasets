ab495f94d702dd476aa7443711759e1f6c8916ed
th3edger <edgarperez_97@hotmail.com>
2022-02-24 07:59:04 -0600

    Se implementa Pathlib para el proceso de Exrtact

--- extract/main.py
+++ extract/main.py
@@ -1,7 +1,7 @@
 import logging
-import os
 import re
 import shutil
+import pathlib
 import wget
 
 import page_object as po
@@ -16,21 +16,28 @@ logger = logging.getLogger(__name__)
 def run():
 
     cities = list(config()['municipios'].keys())
+    CURRENT_DIR = pathlib.Path().resolve()
+    TRANSFORM_DIR = CURRENT_DIR.parent.joinpath("transform").resolve()
 
 
     for city in cities:
-        monterrey = po.tr_Page(city)
 
         logging.info(f'Empezando el proceso para el municipio de {city}')
+        monterrey = po.tr_Page(city)
 
-        if not os.path.isdir(f'./archivos-{city}/'):
-            os.mkdir(f'./archivos-{city}/')
-            os.mkdir(f'./archivos-{city}/years/')
+
+        CITY_FILES_PATH = pathlib.Path(CURRENT_DIR.joinpath(f"archivos-{city}"))
+
+
+        if not CITY_FILES_PATH.exists():
+            CITY_FILES_PATH.mkdir()
+            CITY_FILES_PATH.joinpath("years").mkdir()
+
 
         for year in monterrey.years:
-            if not os.path.isdir(f'./archivos-{city}/years/{year}/'):
-                os.mkdir(f'./archivos-{city}/years/{year}')
-                os.mkdir(f'./archivos-{city}/years/{year}/archivos')
+            if not CITY_FILES_PATH.joinpath("years", str(year)).exists():
+                CITY_FILES_PATH.joinpath("years", str(year)).mkdir()
+                CITY_FILES_PATH.joinpath("years", str(year), "archivos").mkdir()
 
 
         for i,j in zip(monterrey.nom_documentos, monterrey.links_documentos):
@@ -39,9 +46,11 @@ def run():
             document_year = ''.join(document_year)
             wget.download(
                 url=j,
-                out=f'./archivos-{city}/years/{document_year}/archivos/{i}.xlsx'
+                out= str(CITY_FILES_PATH.joinpath("years", str(document_year), "archivos")) + str(f'/{i}.xlsx')
             )
-        shutil.move(f'./archivos-{city}', '../transform/')
+        
+        logging.info(f'\nSe movieron los archivos de la Ciudad de {city} a la carpeta Transform!!!\n')
+        shutil.move(str(CITY_FILES_PATH), str(TRANSFORM_DIR))
 
 
 if __name__ == '__main__':
